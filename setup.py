3c9514749f45886c07f2cf5e4d6740386627fdad
Soumyat686 <128830182+Soumyat686@users.noreply.github.com>
2025-04-18 19:06:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ print(ROOT_DIR)
 REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
 PACKAGE_DIR = ROOT_DIR / 'income_predictor'
 #with open(PACKAGE_DIR / "VERSION") as f:
-_version = 0.0.1
+_version = "0.0.1"
 about["__version__"] = _version
 
 
