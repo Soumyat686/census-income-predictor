085133bb719a632d5bebaf735529d15bfa2dab8f
Soumyat686 <128830182+Soumyat686@users.noreply.github.com>
2025-04-18 18:55:41 +0530

    Update __init__.py

--- income_predictor/__init__.py
+++ income_predictor/__init__.py
@@ -4,6 +4,6 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-from income_predictor.config.core import PACKAGE_ROOT
+#from income_predictor.config.core import PACKAGE_ROOT
 #with open(PACKAGE_ROOT / "VERSION") as version_file:
 __version__ = "0.0.1"
